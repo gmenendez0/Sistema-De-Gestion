773d6f7d3e535e7f372351e553855bb9a0cdcaa8
Guido Menendez <guidomenendez0@gmail.com>
2024-06-24 01:17:49 -0300

    Toques finales

--- src/main/java/com/example/soporte/SoporteApplication.java
+++ src/main/java/com/example/soporte/SoporteApplication.java
@@ -3,10 +3,6 @@ package com.example.soporte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
- * 8. Documentar metodos
- * 9. Ajustar diagrama de clases
- * */
 @SpringBootApplication
 public class SoporteApplication {
 	public static void main(String[] args) {
